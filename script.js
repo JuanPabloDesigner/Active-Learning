f4d66c7bf4a2b1109a7f2f87df332d89fa1a0c36
JuanPabloDesigner <jaypy.uxdesign@gmail.com>
2023-07-23 22:48:06 -0300

    v1 1.1 card style added :star_struck:

---
+++ script.js
@@ -0,0 +1,10 @@
+/* V2
+
+function setProgress(percent) {
+  const progress = document.querySelector('.progress');
+  progress.style.width = percent + '%';
+}
+
+// Example usage: setProgress(50);
+
+*/
